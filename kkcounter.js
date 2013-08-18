7e2d27258a29de6a3d839a1ae789ea29a6974ed2
astrobolidos <lisandroalavez@gmail.com>
2013-08-18 21:15:44 +0100

    reactive graph add

--- kkcounter.js
+++ kkcounter.js
@@ -30,59 +30,53 @@ if (Meteor.isClient) {
 		}
 	});
 
+	Template.graphs.destroyed = function() {
+		if(this.drawGraph) this.drawGraph.stop();
+	};
+
 	Template.graphs.rendered = function() {
 		console.log('graphs.rendered()');
-
 		
-
-		var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
-		               11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
-
-		var div = this.find('#bar');
+		this.node = this.find('#bar');
 		var w = 500;
 		var h = 200;
 		var barPadding = 1;
 
-		var svg = d3.select(div)
-		    .append("svg")
-		    .attr("width", w)
-		    .attr('height', h);
+		var svg = d3.select(this.node)
+			.append("svg")
+			.attr("width", w)
+			.attr('height', h);
 
-		svg.selectAll('rect')
-			.data(dataset)
-			.enter()
-			.append("rect")
-			.attr("x", function(d, i) {
-				return i * (w / dataset.length);  //Bar width of 20 plus 1 for padding
-			})
-			.attr("y", function(d) {
-				return h - (d*4);  //Height minus data value
-			})
-			.attr("width", w / dataset.length - barPadding)
-			.attr("height", function(d) {
-				return d * 4;
-			})		
-			.attr("fill", function(d) {
-			    return "rgb(0, 0, " + (d * 10) + ")";
-			});
+		if(!this.drawGraph) {
+			this.drawGraph = Deps.autorun(function(){
+				console.log('Deps.autorun');
 
-		svg.selectAll("text")
-			.data(dataset)
-			.enter()
-			.append("text")
-			.text(function(d) {
-				return d;
-			})
-			.attr("x", function(d, i) {
-				return i * (w / dataset.length) + (w / dataset.length - barPadding) / 2;
-			})
-			.attr("y", function(d) {
-				return h - (d * 4) + 14;  //15 is now 14
-			})
-			.attr("text-anchor", "middle")
-			.attr("font-family", "sans-serif")
-			.attr("font-size", "11px")
-			.attr("fill", "white");
+				var info = DailyCalories.find({}, {sort: {date: -1}}).fetch();
+			
+				if(info.length > 0) {
+					svg.selectAll('rect')
+						.data(info, function(i) { 
+							return i.calories; 
+						})
+						.enter()
+						.append("rect")
+						.attr("x", function(d, i) {
+							return i * (w / info.length); //Bar width of 20 plus 1 for padding
+						})
+						.attr("y", function(d) {
+							return h - (d.calories/10); //Height minus data value
+						})
+						.attr("width", w / info.length - barPadding)
+						.attr("height", function(d) {
+							return d.calories / 10;
+						})	
+						.attr("fill", 'teal');
+				}
+
+
+
+			});	
+		}
 	};
 }
 
