26e8260f6560ffcb8a3a74baa830cfbfdfd0948b
astrobolidos <lisandroalavez@gmail.com>
2013-09-06 23:29:17 +0100

    update styles

--- client/calorieGraph.js
+++ client/calorieGraph.js
@@ -22,16 +22,16 @@ if(Meteor.isClient) {
 			self.drawGraph = Deps.autorun(function(){
 				var info = DailyCalories.find({}, {sort: {date: 1}}).fetch();
 				var barPadding = 1;
-				var w = 200;
-				var h = 200;
+				var w = 900;
+				var h = 100;
 				
 				var updateBar = function(bar) {
 					bar.attr("id", function (d) { return d._id; })
 					.attr('data-calories', function(d) { return d.calories; })
 					.attr("x", function(d, i) { return i * (w / info.length); })//Bar width of 20 plus 1 for padding
-					.attr("y", function(d) { return h - (d.calories / 10); }) //Height minus data value
+					.attr("y", function(d) { return h - (d.calories / 20); }) //Height minus data value
 					.attr("width", w / info.length - barPadding)
-					.attr("height", function(d) { return d.calories / 10; })	
+					.attr("height", function(d) { return d.calories / 20; })	
 					.attr("fill", function(d) { 
 						var colour = 'gray';
 						if(d.calories > 1699) colour = 'green';
